4c3e9fd834df17ab63fb4206b467412a2ae19199
dahunkim <kim23222@naver.com>
2020-03-20 20:22:48 +0900

    [추가] Mail Service 설정
    메일 본문에 이미지 첨부 설정 테스트

--- HotelDalluna/src/main/java/com/java/service/MailServiceImplment.java
+++ HotelDalluna/src/main/java/com/java/service/MailServiceImplment.java
@@ -1,16 +1,44 @@
 package com.java.service;
 
+import java.io.File;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.internet.MimeBodyPart;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import com.java.mail.MailUtils;
 
 @Service
 public class MailServiceImplment implements MailService{
 	
-	@Autowired JavaMailSender mailsender; 
+	@Autowired
+	private JavaMailSender mailSender;
 	
 	@Override
-	public void sendEmail(String contnet) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void sendEmail(String content) throws Exception {
+		BodyPart messageBodyPart = new MimeBodyPart();
+		//현재 경로
+		File f = new File(".");
+		//추가 파일 있는 경로
+		DataSource image = new FileDataSource(f+"\\src\\main\\webapp\\resources\\images\\favicon\\hotelDalluna384x384.jpg");
+		messageBodyPart.setDataHandler(new DataHandler(image));
+		messageBodyPart.setHeader("Content-ID","<image>");
+		System.out.println("확인!!!!!!!!!!");
+		MailUtils sendMail = new MailUtils(mailSender);
+		sendMail.setSubject("이메일 확인 인증 test 입니다.");
+		sendMail.setText(new StringBuffer().append("<img src=\"cid:image\">")
+										   .append("<h1>HotelDalluna 아이디 비밀번호 찾기</h1>")
+										   .append("test중입니다.").toString());
+		sendMail.addInLine("image", image);
+		sendMail.setForm("dahunkim90@gmail.com", "HotelDalluna");
+		sendMail.setTo("kim23222@naver.com");
+		sendMail.send();
 	}
+	
 }
